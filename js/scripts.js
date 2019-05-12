0356b0ba8f0f3d560de0ca269571ce3f3333d42f
James Lin <james.lin1314520@outlook.com>
2019-05-12 22:27:20 +0930

    Fixed back to top button appearing when website first loads and before any scrolling occurs
    Signed-off-by: James Lin <james.lin1314520@outlook.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,10 @@
 //Back to top button script
+
 window.onscroll = function() {scrollFunction()};
 
-function scrollFunction() {
-    document.getElementById("fixedbutton").style.display = "none";
+document.getElementById("fixedbutton").style.display = "none";
 
+function scrollFunction() {
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
         document.getElementById("fixedbutton").style.display = "block";
     }
