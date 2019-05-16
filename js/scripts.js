2625362ed4569256a4d3a5f5f1e2edf1861c5204
allenjinrongzhang <az.cdu.au@gmail.com>
2019-05-16 22:54:02 +0930

    update cards, navbar and picture
    Signed-off-by: allenjinrongzhang <az.cdu.au@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,7 @@
 window.onscroll = function() {scrollFunction()};
 
 document.getElementById("fixedbutton").style.display = "none";
+document.getElementById("floatBar").style.display = "none";
 
 function scrollFunction() {
     if (document.body.scrollTop > 190 || document.documentElement.scrollTop > 190) {
